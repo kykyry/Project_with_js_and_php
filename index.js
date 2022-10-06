8782d0cbb89884c1d256de5d0aa797cf041661b6
Maria Vasyutina <masha.vasutina.1998@gmail.com>
2022-10-06 16:34:01 +0300

    js

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ function modifyBackground() {
 
     switch (document.querySelector("#type").value) {
         case 'linear':
+            console.log("l")
             block.style.background = `linear-gradient(to left, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)})`;
-            info.innerHTML = `Background: ${block.style.background}`;
+            info.innerHTML = `Background: linear-gradient(#${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)})`;
             break;
 
         case 'conic':
+            console.log("c")
             block.style.background = `conic-gradient(#${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)})`;
-            info.innerHTML = `Background: ${block.style.background}`;
+            info.innerHTML = `Background: conic-gradient(#${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)})`;
             break;
 
         case 'radial':
+            console.log("r")
             block.style.background = `radial-gradient(#${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)})`;
-            info.innerHTML = `Background: ${block.style.background}`;
+            info.innerHTML = `Background: radial-gradient(#${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}, #${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)}${getRandomInt(0, 255).toString(16)})`;
             break;
     }
 
     function getRandomInt(min, max) {
-        // min = Math.ceil(min);
-        // max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
     }
 }
